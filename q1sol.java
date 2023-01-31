30e3e85db2342d3ea8f85c54cca1a0bf70d35e1b
Sedvin1903 <83063914+Sedvin1903@users.noreply.github.com>
2023-01-31 23:47:17 +0530

    Update q1sol.java

--- q1sol.java
+++ q1sol.java
@@ -6,18 +6,18 @@ class Main {
     public static void main (String[] args) {
 
       try 
-       {
+       {   /* get i/p from user and convert to lowercase */
          Scanner sc =  new Scanner(System.in);
          String s = " ";
          s = sc.nextLine();
          s.toLowerCase();
      
         int a=0 ;
-        a = s.lastIndexOf('a');
+        a = s.lastIndexOf('a');       /* Check the last occurence  of 'a' in string */
         if( a == -1)
           System.out.println("-1");
         else
-         System.out.println(a+1);
+         System.out.println(a+1);  /* Since index  was conditioned to be start from 1 */
        }
       catch(Exception e)
       {
