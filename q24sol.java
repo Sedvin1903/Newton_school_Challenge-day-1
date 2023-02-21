cc0caf0002b7c43660f3acae12fb31101d3c031a
Sedvin1903 <83063914+Sedvin1903@users.noreply.github.com>
2023-02-21 22:43:14 +0530

    Create q24sol.java

---
+++ q24sol.java
@@ -0,0 +1,19 @@
+import java.io.*; 
+import java.util.*; 
+
+/* REFER README FOR QUESTION */
+
+class Main {
+
+public static void main (String[] args) {
+
+   Scanner sc = new Scanner (System.in);
+   String S = sc.nextLine();
+
+   if(S.endsWith("er"))
+     System.out.println("er");
+   if(S.endsWith("ist"))
+     System.out.println("ist");
+
+  }
+}
